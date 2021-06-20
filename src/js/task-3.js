b7d1f4de69200650df33a32d9a979f752caf45ce
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-06-20 16:54:35 +0300

    Update task-3.js

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -20,8 +20,8 @@ const makeTransaction = transaction => {
   });
 };
 
-const logSuccess = result => {
-  console.log(`Transaction ${result.id} processed in ${result.delay}ms`);
+const logSuccess = ({ id, delay }) => {
+  console.log(`Transaction ${id} processed in ${delay}ms`);
 };
 
 const logError = id => {
